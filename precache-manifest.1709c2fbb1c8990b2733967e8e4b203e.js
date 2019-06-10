7eb320316448f97e5ee29cf77f50770318bb06dc
BURCHILL <JBURCHILL@wrps.on.ca>
2019-06-10 08:30:28 -0400

    Updates

---
+++ precache-manifest.1709c2fbb1c8990b2733967e8e4b203e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609782ac8283d340cbe628a7df381337",
+    "url": "/RecipeSearchReactApp/index.html"
+  },
+  {
+    "revision": "9b6780b0fc8a6391a537",
+    "url": "/RecipeSearchReactApp/static/css/main.f6fa9ac3.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/RecipeSearchReactApp/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "9b6780b0fc8a6391a537",
+    "url": "/RecipeSearchReactApp/static/js/main.bd53f3d1.chunk.js"
+  },
+  {
+    "revision": "05042d2986fa4089ce3e",
+    "url": "/RecipeSearchReactApp/static/js/runtime~main.c20c670a.js"
+  }
+]);
